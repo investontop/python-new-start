d2e783e5ba3c5aba5a61681b4007a9999c44cae7
Ramanathan <investontop@gmail.com>
2025-03-29 21:10:34 +0530

    Update using VSC

--- JSON to XML/JsonToXml.py
+++ JSON to XML/JsonToXml.py
@@ -31,3 +31,4 @@ xml_tree.write("output.xml", encoding="utf-8", xml_declaration=True)
 
 print("XML conversion complete. Output saved to {}\output.xml.".format(current_directory))
 print('')
+print('Updated using VSC')
